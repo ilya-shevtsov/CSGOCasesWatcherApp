d7448e6f83e0d6bc9b90c3203b0bfb9a94f40e70
ilya-shevtsov <Dayzilya@gmail.com>
2021-03-16 18:00:48 +0300

    CasesPreviewFragment.getCaseList: Added extension method for "toListOfCaseDto" Added caseDtoToCase function

--- app/src/main/java/com/example/csgocaseswatcherapp/view/CasesPreviewFragment.kt
+++ app/src/main/java/com/example/csgocaseswatcherapp/view/CasesPreviewFragment.kt
@@ -9,8 +9,11 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.example.csgocaseswatcherapp.R
 import com.example.csgocaseswatcherapp.common.disposeOnDestroy
+import com.example.csgocaseswatcherapp.data.model.Case
+import com.example.csgocaseswatcherapp.data.model.CaseDto
 import com.example.csgocaseswatcherapp.utils.ApiTools
 import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
@@ -45,21 +48,15 @@ class CasesPreviewFragment : Fragment() {
         getCaseList()
     }
 
-    private val caseNameList = listOf("Glove%20Case","Operation%20Breakout%20Weapon%20Case")
+    private val caseNameList = listOf("Glove%20Case", "Operation%20Breakout%20Weapon%20Case")
 
 
     private fun getCaseList() {
         Observable.just(caseNameList)
-            .flatMap { caseNameList -> Observable.fromIterable(caseNameList) }
-            .flatMap { caseName ->
-                ApiTools.getApiService()
-                    .getCase(
-                        appId = 730,
-                        currency = 5,
-                        caseName = caseName
-                    ).toObservable()
+            .toListOfCastDto()
+            .map { listOfCaseDto ->
+                listOfCaseDto.map { caseDto -> caseDtoToCase(caseDto) }
             }
-            .toList()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeBy(
@@ -73,4 +70,42 @@ class CasesPreviewFragment : Fragment() {
                 })
             .disposeOnDestroy(viewLifecycleOwner)
     }
+
+    fun caseDtoToCase(caseDto: CaseDto, caseName:String): Case {
+
+        val newCaseName = caseName
+            .replace("%20"," ")
+
+        val newLowestPrice = caseDto.lowestPrice
+            .replace(" pуб.", "")
+            .replace(",", ".")
+            .toFloat()
+
+        val newVolume = caseDto.volume
+            .replace(",", "")
+            .toInt()
+
+        val newMedianPrice = caseDto.medianPrice
+            .replace(" pуб.", "")
+            .replace(",", ".")
+            .toFloat()
+        return Case(
+            name = newCaseName,
+            lowestPrice = newLowestPrice,
+            volume = newVolume,
+            medianPrice = newMedianPrice
+        )
+    }
+
+    fun Observable<List<String>>.toListOfCastDto(): Single<List<CaseDto>> =
+        flatMap { caseNameList -> Observable.fromIterable(caseNameList) }
+            .flatMap { caseName ->
+                ApiTools.getApiService()
+                    .getCase(
+                        appId = 730,
+                        currency = 5,
+                        caseName = caseName
+                    ).toObservable()
+            }
+            .toList()
 }
